e42fb86a4ae1bb37d313afaa6de04843171ccf01
jamday <jgascoignetaylor@godaddy.com>
2015-07-31 18:26:05 +0200

    added styling

--- php-mimistory/promotions.php
+++ php-mimistory/promotions.php
@@ -17,6 +17,38 @@
     foreach ($promotions as $promotion) {
         if ($promotion['hidden'] == 'false'){
         ?>
+        <style type="text/css">
+            BODY{
+                font-family: sans-serif;
+                font-size:12px;
+                color:#c2c2c2;
+            }
+            #promotionsDiv{
+                clear:both;
+                height:54px;
+                margin-left:10px;
+                margin-top:10px;
+            }
+            #promotionsDiv img{
+                height:44px; 
+                overflow:hidden;
+                float:left
+            }
+            .textDiv{
+                float:left;
+                margin-left:10px
+            }
+            #promotionsHeading{
+                margin:0; 
+                margin-bottom:3px;
+                padding:0;
+            }
+            #promotionsLink{
+                font-family: sans-serif;
+                font-size:12px;
+                text-decoration:none;
+            }
+        </style>
             <div id="promotionsDiv">
                 <img src="<?php echo $promotion['thumbnail']; ?>" alt="<?php echo $promotion['thumbnail']; ?>" />
                 <div class="textDiv">
@@ -24,7 +56,6 @@
                     Link: <a id="promotionsLink" href="http://www.madmimi.com/p/<?php echo $promotion['mimio']; ?>" target="_blank" title="Created on <?php echo $promotion['updated_at']; ?>">http://www.madmimi.com/p/<?php echo $promotion['mimio']; ?></a><br />
                 </div>
             </div>
-s
             <?php
         }
     }
